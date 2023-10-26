a9bb33dae661eac18ca5fbd05f46b63c36b62146
corinabataiosu <149093501+corinabataiosu@users.noreply.github.com>
2023-10-26 23:03:54 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,3 @@
-/*(Tipul bool) Se citete de la tastatura o secventa de siruri de caractere. Sa se dezvolte o
-aplicatie care determina apartenenta unui alt sir de caractere la secventa, iar în cazul in
-care sirul apartine secventei sa se contorizeze numarul de aparitii ale acestuia. Secventa
-se memoreaza intr-un tablou de siruri.*/
 #include <iostream>
 #include <string>
 
